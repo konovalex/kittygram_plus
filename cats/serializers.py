1f09993b95d1d2c46ef1aeeb997a0cd624a1e744
Konov Aleksey <konov.alexey@gmail.com>
2024-05-21 18:11:09 +0300

    Create own type field Hex2NameColor in serializers.py

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,6 +1,21 @@
+import datetime as dt
+
 from rest_framework import serializers
+import webcolors
 
 from .models import Cat, Owner, Achievement, AchievementCat
+
+
+class Hex2NameColor(serializers.Field):
+    def to_representation(self, value):
+        return value
+
+    def to_internal_value(self, data):
+        try:
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            raise serializers.ValidationError('Для того цвета нет имени')
+        return data
 
 
 class AchievementSerializer(serializers.ModelSerializer):
@@ -12,10 +27,17 @@ class AchievementSerializer(serializers.ModelSerializer):
 
 class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    color = Hex2NameColor()
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = (
+            'id', 'name', 'color', 'birth_year', 'owner', 'achievements', 'age'
+        )
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
 
     def create(self, validated_data):
         if 'achievements' not in self.initial_data:

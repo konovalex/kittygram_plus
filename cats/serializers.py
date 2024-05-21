754abf7d64e861fb72ab7d5cf4db6897c9106b3f
Konov Aleksey <konov.alexey@gmail.com>
2024-05-21 18:45:10 +0300

    Addition settings in serializer

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,34 +1,21 @@
 import datetime as dt
 
 from rest_framework import serializers
-import webcolors
 
 from .models import Cat, Owner, Achievement, AchievementCat
 
 
-class Hex2NameColor(serializers.Field):
-    def to_representation(self, value):
-        return value
-
-    def to_internal_value(self, data):
-        try:
-            data = webcolors.hex_to_name(data)
-        except ValueError:
-            raise serializers.ValidationError('Для того цвета нет имени')
-        return data
-
-
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
     age = serializers.SerializerMethodField()
-    color = Hex2NameColor()
 
     class Meta:
         model = Cat
